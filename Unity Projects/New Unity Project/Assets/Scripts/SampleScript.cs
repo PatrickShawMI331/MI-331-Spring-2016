b656222a4cdb76b7ef329a2d6783409f6df8ff95
PatrickShawMI331 <shawpat01@gmail.com>
2016-01-01 10:56:41 -0500

    First commit
    description of changes

---
+++ Unity Projects/New Unity Project/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("test this code2");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
